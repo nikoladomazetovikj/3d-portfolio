81156fa11456addbe92d069d9857917c16d56ec7
nikoladomazetovikj <nikolad150@gmail.com>
2024-02-11 02:22:47 +0100

    fix: reset initial camera position

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+
+camera.position.set(20, 0, 5);
+camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
